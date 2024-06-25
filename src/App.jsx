7246f4e10117b0f66055d93025b85d73ce118ffd
mbr-dev <dev@mbr.net.br>
2024-06-25 08:35:16 -0300

    add config server

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Router } from './router';
 
 export function App() {
   return (
-   <BrowserRouter>
+   <BrowserRouter basename={'/game/JAM/app/'}>
     <Router />
    </BrowserRouter>
   )
