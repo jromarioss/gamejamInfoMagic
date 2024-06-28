2d3a51b3388dcc482ebeeb4fd8ade59c36ee48b4
mbr-dev <dev@mbr.net.br>
2024-06-28 10:57:08 -0300

    ajuste no circulo do mp

--- src/pages/Home/components/Circle/index.jsx
+++ src/pages/Home/components/Circle/index.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
 const Circle = ({ value }) => {
   return (
-    <div className="ml-6 flex items-center justify-center h-16 w-16 rounded-full bg-blue-500 text-white text-sm font-bold z-10">
+    <div className="flex items-center justify-center h-10 w-10 rounded-full bg-blue-500 text-white text-sm font-bold z-10">
       {value}
     </div>
   );
